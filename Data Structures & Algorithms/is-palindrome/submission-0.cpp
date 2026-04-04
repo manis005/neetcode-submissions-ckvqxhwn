4d51da9df188fd5f63c142edeebb44f84f2e8be2
moz <42327152+manis005@users.noreply.github.com>
2026-04-04 23:05:21 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAlphanumeric(char ch) {
+        return (ch >= 'A' && ch <= 'Z') ||
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= '0' && ch <= '9');
+    }
+
+    bool isPalindrome(string s) {
+        int l = 0, r = s.length() - 1;
+        while(l < r) {
+            if (!isAlphanumeric(s[l])) {
+                l++;
+                continue;
+            }
+            if (!isAlphanumeric(s[r])) {
+                r--;
+                continue;
+            }
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+};
